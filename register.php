ee5c760ce6fffbe88be9ff542d686543a1b2d352
TijmenSosef <73581008+TijmenSosef@users.noreply.github.com>
2021-02-01 11:33:18 +0100

    -Ajax fix

--- register.php
+++ register.php
@@ -7,7 +7,7 @@ session_start();
     <meta charset="UTF-8">
     <title>Login</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
-    <script src="ajax.js"></script>
+    <script src="js/ajax.js"></script>
 </head>
 <body>
 <form onsubmit="return Register();" method="post">
