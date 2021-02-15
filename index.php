ec932a97db7a28cd7294ba2b4abda22b41fb96dd
CristianContant <73581046+CristianContant@users.noreply.github.com>
2021-02-15 12:42:25 +0100

    Sidebar changes, kon niet goed klikken

--- index.php
+++ index.php
@@ -137,8 +137,8 @@ session_start();
 
   <!-- <?php if(SESSION['ID_User']) { ?><button name="Logout"></button> <?php } else { ?> <button name="Login"></button> <?php } ?>  -->
   <!-- Sidebar -->
-  <aside class="col-12 col-md-2 p-0 flex-shrink-1">
-    <nav class="navbar navbar-expand flex-row align-items-start py-2 sidebar">
+  <aside class="col-12 col-md-1 p-0 flex-shrink-1 sidebar">
+    <nav class="navbar navbar-expand flex-row align-items-start py-2">
       <div class="collapse navbar-collapse ">
         <ul class="flex-md-column flex-row navbar-nav w-100 justify-content-between">
           <p>Followed Channels</p>
