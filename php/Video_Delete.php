e2114199ad410ac2f3cba7f89d2e4dd142d21f2a
TijmenSosef <73581008+TijmenSosef@users.noreply.github.com>
2021-02-15 12:55:41 +0100

    -added security to Delete and Rename

--- php/Video_Delete.php
+++ php/Video_Delete.php
@@ -1,12 +1,12 @@
 <?php
-$_SESSION['ID_User'];
+session_start();
 require 'config.php';
 
 $ID_Video = $_POST['ID_Video'];
 
 
-$Getinfo = $mysqli->prepare("SELECT Video FROM video WHERE ID_Video = ?");
-$Getinfo -> bind_param('i', $ID_Video);
+$Getinfo = $mysqli->prepare("SELECT Video FROM video WHERE ID_Video = ? AND ID_User = ?");
+$Getinfo -> bind_param('ii', $ID_Video,$_SESSION['ID_User']);
 $Getinfo -> execute();
 $Getinfo -> bind_result($Path);
 $Getinfo -> fetch();
@@ -14,8 +14,8 @@ $Getinfo -> close();
 $Path = "../upload/".$Path;
 
 if(is_numeric($ID_Video)){
-    $DeleteVideo = $mysqli->prepare("DELETE FROM video WHERE ID_Video = ?");
-    $DeleteVideo -> bind_param('i', $ID_Video);
+    $DeleteVideo = $mysqli->prepare("DELETE FROM video WHERE ID_Video = ? AND ID_User = ?");
+    $DeleteVideo -> bind_param('ii', $ID_Video, $_SESSION['ID_User']);
     if($DeleteVideo -> execute()){
         echo "success";
         unlink($Path);
--- php/Video_Rename.php
+++ php/Video_Rename.php
@@ -1,12 +1,13 @@
 <?php
 require 'config.php';
 
+session_start();
 $Title = $_POST['Title'];
 $ID_Video = $_POST['ID_Video'];
 
 
-$Rename = $mysqli -> prepare("UPDATE video SET Title=? WHERE ID_Video=?");
-$Rename -> bind_param('si', $Title, $ID_Video);
+$Rename = $mysqli -> prepare("UPDATE video SET Title=? WHERE ID_Video=? AND ID_User = ?");
+$Rename -> bind_param('sii', $Title, $ID_Video, $_SESSION['ID_User']);
 if ($Rename -> execute()){
     echo "success";
 }else{
