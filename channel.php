7b7d1d88773d70ef841c7998b3e5c491d50e6eb1
JoeriDekker <73581036+JoeriDekker@users.noreply.github.com>
2021-02-15 14:11:46 +0100

    Update channel.php

--- channel.php
+++ channel.php
@@ -10,7 +10,7 @@ require "php/config.php";
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Homepage</title>
+  <title>Channel</title>
 
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
