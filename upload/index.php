07b2a814f8efd27c0b9c4c4df66e35aef9711b5c
CristianContant <73581046+CristianContant@users.noreply.github.com>
2021-02-28 17:06:42 +0100

    Styling add video index.php

--- upload/index.php
+++ upload/index.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-
 <?php
 session_start();
 require '../php/config.php';
@@ -9,23 +8,44 @@ if ( $_SESSION['Loggedin'] !== true) {
 }
 
 ?>
-
 <head>
     <title>Dashboard</title>
+    <!-- Jquery link -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 
+    <!-- Bootstrap Link -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="../css/dashboard.css">
+
+    <!-- JS link -->
+    <script src="js/ajax.js"></script>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
 </head>
 
 <body>
 
-<form action="upload_process.php" method="post" enctype="multipart/form-data">
-    Select image to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload"><br><br>
-    tittle:
-    <input type="text" name="title"><br><br>
+    <form class="formVideo" action="upload_process.php" method="post" enctype="multipart/form-data">
+        <p>Select video to upload:</p>
+        <input type="file" name="fileToUpload" id="fileToUpload"><br><br>
+        <p>Title:</p>
+        <input type="text" name="title"><br><br>
 
-    <select name="catagory" id="catagory">
+        <select name="catagory" id="catagory">
 
-        <?php
+            <?php
         $statement = $mysqli -> prepare("SELECT ID_Catagory, Catagory FROM `catagory`");
         $statement -> execute();
         $result = $statement->get_result();
@@ -33,11 +53,12 @@ if ( $_SESSION['Loggedin'] !== true) {
 
             ?>
             <option value="<?php echo $row['ID_Catagory'];?>"><?php echo $row['Catagory'];?></option>
-        <?php }?>
-    </select>
+            <?php }?>
+        </select>
 
-    <input type="submit" value="Upload Image" name="submit">
-</form>
+        <input type="submit" value="Upload Image" name="submit">
+    </form>
 
 </body>
+
 </html>
