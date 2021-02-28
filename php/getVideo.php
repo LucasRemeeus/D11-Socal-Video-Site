b68326d4d0f9a60dc39a08a7a247189918517bf1
CristianContant <73581046+CristianContant@users.noreply.github.com>
2021-02-28 18:46:06 +0100

    Yeetus

--- php/getVideo.php
+++ php/getVideo.php
@@ -20,10 +20,14 @@ $getVideoResult = $getVideo -> get_result();
 while ($Video = $getVideoResult -> fetch_assoc())
 {
     ?>
-            <a href="watch.php?watch=<?php echo $Video['ID_Video'] ?>"><div class="col-md vid">
-                <h2><?php echo $Video['Title'] ?></h2>
-                <video width="140%" src="upload/<?php echo $Video['Video']?>" ></video>
-            </div></a>
+        <a href="watch.php?watch=<?php echo $Video['ID_Video'] ?>">
+            <div class="col-md vid">
+                <div class="titelText">
+                    <h2><?php echo $Video['Title'] ?></h2><br>
+                </div>
+                <video width="100%" src="upload/<?php echo $Video['Video']?>"></video>
+            </div>
+        </a>
     <?php
 }
 
