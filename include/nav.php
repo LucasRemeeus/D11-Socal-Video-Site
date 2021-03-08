1b69a2ef5275632c9f1275bb1f6cc1567cce13ef
CristianContant <73581046+CristianContant@users.noreply.github.com>
2021-03-08 13:12:47 +0100

    Profile picture automatisch in de navbar

--- include/nav.php
+++ include/nav.php
@@ -1,3 +1,40 @@
+<?php
+require 'php/config.php';
+$GetUserData = $mysqli->prepare("SELECT * FROM user WHERE ID_User = ?");
+$GetUserData->bind_param('i', $_SESSION['ID_User']);
+$GetUserData->execute();
+$GetUserDataResult = $GetUserData->get_result();
+$UserRow = $GetUserDataResult->fetch_assoc()
+?>
+<!DOCTYPE html>
+
+<head>
+  <title>Navbar</title>
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+    integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+    integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg=="
+    crossorigin="anonymous"></script>
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/channelinfo.css">
+
+  <!-- De font -->
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+
+  <!-- Jquery link -->
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+    integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <!-- JS link -->
+  <script src="js/ajax.js"></script>
+</head>
+
+
+
+<body>
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg nav">
     <a class="navbar-brand" href="index.php">
@@ -24,8 +61,16 @@
       </li>
       <li class="nav-item">
         <a class="navbar-brand" href="channel.php">
-          <img class="logo" src="img/TwotchLogo.png" alt="Profile Logo">
+        <?php if($_SESSION['Loggedin'] == true ) 
+        { ?>
+          <img class="logo" id="preview" src="upload/profilepicture/<?php echo $UserRow['ProfilePicture'] ?>" alt="Profile Logo"></a>
+          <?php } 
+        else 
+        { ?> <a class="navbar-brand" href="#"><img class="logo" src="img/TwotchLogo.png" alt="Twotch Logo"> <?php } ?>
         </a>
       </li>
     </ul>
   </nav>
+  </body>
+
+</html>
