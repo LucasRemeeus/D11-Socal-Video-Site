5927639a78a7eb72e412ee22a415d9cb8f595c5b
CristianContant <73581046+CristianContant@users.noreply.github.com>
2021-03-15 13:09:04 +0100

    Dashboard.php bijna af, alleen nog aantal comments

--- dashboard.php
+++ dashboard.php
@@ -12,46 +12,69 @@ $GetUserDataResult = $GetUserData->get_result();
     while ($UserRow = $GetUserDataResult->fetch_assoc()) {
 
 ?>
-
 <!DOCTYPE html>
+<html lang="en">
 
 <head>
-    <title>dashboard</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+        integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg=="
+        crossorigin="anonymous"></script>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/dashboard.css">
+
+    <!-- De font -->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
 
     <!-- Jquery link -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"
         integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 
-    <!-- Bootstrap Link -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <!-- CSS -->
-    <link rel="stylesheet" href="css/dashboard.css">
-
-    <!-- JS link -->
+    <!-- Ajax -->
     <script src="js/ajax.js"></script>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
-    </script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
-    </script>
-
-
 </head>
 
 <body onload="DashboardVids()">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous">
-    </script>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg nav">
+        <a class="navbar-brand" href="index.php">
+            <img class="logo" src="img/TwotchLogo.png" alt="TwotchLogo">
+        </a>
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link link active" href="channel.php">Back</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                    <?php if($_SESSION['Loggedin'] == true ) 
+        { ?>
+                    <img class="logo" id="preview" src="upload/profilepicture/<?php echo $UserRow['ProfilePicture'] ?>"
+                        alt="Profile Logo"></a>
+                <?php } 
+        else 
+        { ?> <a class="navbar-brand" href="#"><img class="logo" src="img/TwotchLogo.png" alt="Twotch Logo"> <?php } ?>
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <!-- Sidebar -->
+    <?php
+    include "include/sidebar.php";
+  ?>
 
-    <!--TODO: MODAL voor de rename-->
-    <div class="titleBackground">
-        <div><a class="nav-link button button-link button-login" href="channel.php">Back to your channel</a></div><br>
+    <!-- Main content -->
+    <div class="container-fluid col-10 mt-3 container">
+        <h1>Uploads</h1>
+
         <div id="resultaat"></div>
-        <div class="error" id="error"></div>
     </div>
 
 </body>
