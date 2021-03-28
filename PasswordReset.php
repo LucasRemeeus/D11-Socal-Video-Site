d7df2fb9a46fe9d2db594e18f6c2c406a68b626c
JoeriDekker <73581036+JoeriDekker@users.noreply.github.com>
2021-03-28 20:31:01 +0200

    passwordreset style

--- PasswordReset.php
+++ PasswordReset.php
@@ -1,7 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reset Password</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+        integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg=="
+        crossorigin="anonymous"></script>
 
-<form method="post" action="php/Password_link.php">
-    Email:
-    <input name="Email" type="email" ID="Email"><br>
-    <input name="Submit" type="Submit" ID="Submit">
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/login.css">
+
+    <!-- De font -->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+
+    <!-- Jquery link -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
+    <!-- Ajax -->
+    <script src="js/ajax.js"></script>
+</head>
+<body>
+
+<form class="login-form" method="post" action="php/Password_link.php">
+<img src="img/TwotchLogo.png"><h1>Reset Password</h1>
+<div class="textb">
+            <p>Email</p>
+            <input name="Email" type="email" ID="Email" required>
+            <br><br>
+            
+        
+        </div>
+    <input class="btn fas fa-arrow-right" name="Submit" type="Submit" ID="Submit">
 </form>
+    
+</body>
+</html>
 <?php
